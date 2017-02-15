3efe2b6778f49ad124cfc50b1dada706f548f692
rishabmehra <rishabmehra10@gmail.com>
2017-02-15 13:34:06 +0530

    Add files via upload
    converted code to ES6 and remove the mixins property

---
+++ Vote.jsx
@@ -0,0 +1,37 @@
+import React,{Component,PropTypes } from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+export default class Vote extends React.Component{
+  constructor(props){
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+  getPair() {
+    return this.props.pair || [];
+  }
+  isDisabled() {
+    return !!this.props.hasVoted;
+  }
+  hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  }
+  render() {
+    return <div className="voting">
+      {this.getPair().map(entry =>
+        <button key={entry}
+                disabled={this.isDisabled()}
+                onClick={() => this.props.vote(entry)}>
+          <h1>{entry}</h1>
+          {this.hasVotedFor(entry) ?
+            <div className="label">Voted</div> :
+            null}
+        </button>
+      )}
+    </div>;
+  }
+}
+Vote.propTypes = {
+  pair: PropTypes.any,
+  hasVoted: PropTypes.string
+};
+ 
